4ec5dc8146051af03b4cbcdd70f68564252159a7
Evcoder <evanna0309@outlook.com>
2025-05-11 15:58:25 +0530

    Update code.py
    Code

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
+print('Hello')
+print(''' I see you are here. I'm glad.''')
+name = input(' Please enter your beautiful name.').upper()
+if name == disha:
+  print(' Welcome, you have been authorized.')
+  print(''' Your next clue, is this: 'They were all bound, by the silken thread of destiny'. You'll need a brain to understand this.''')
+elif name == directiond:
+  print(' Welcome, you have been authorized.')
+  print(''' Your next clue, is this: 'They were all bound, by the silken thread of destiny'. You'll need a brain to understand this.''')
+else:
+  print(' You are not authorized. Please enter your name again, if you made any mistakes.')
 
